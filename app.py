378b9e7f06e7fb8530c6dfb63956487f347b38e8
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-07-02 01:34:54 +0530

    parapager-2 finalising and running tests on the flask server.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 import os   
 app = Flask(__name__)
 CORS(app) 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyAPTMyEWrmeaUU-OgxO08fShS1YhFObFkY"
+os.environ["GOOGLE_API_KEY"] = "API-KEY"
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 @app.route('/api', methods=['POST'])
@@ -17,3 +17,29 @@ def generate_summary():
     loader = WebBaseLoader(site)
     docs = loader.load()
     
+    doc_prompt = PromptTemplate.from_template("{page_content}")
+    llm_prompt_template = """Write a concise summary of the following:
+"{text}"
+CONCISE SUMMARY:"""
+    llm_prompt = PromptTemplate.from_template(llm_prompt_template)
+    
+    llm = ChatGoogleGenerativeAI(model="gemini-pro",
+                                 temperature=0.7, top_p=0.85)
+    
+    stuff_chain = (
+        {
+            "text": lambda docs: "\n\n".join(
+                format_document(doc, doc_prompt) for doc in docs
+            )
+        }
+        | llm_prompt         # Prompt for Gemini
+        | llm                # Gemini function
+        | StrOutputParser()  # output parser
+    )
+    
+    response = stuff_chain.invoke(docs)
+    
+    return jsonify({"summary": response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
